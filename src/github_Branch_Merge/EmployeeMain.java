c369ca06209bd9a7488eb096671700109a123cd8
abdoulayekaloga <Abdoulaye01>
2019-04-12 19:29:30 +0100

    This branch is created in extension to the master branch which then be used to merge with the master branch.

--- src/github_Branch_Merge/EmployeeMain.java
+++ src/github_Branch_Merge/EmployeeMain.java
@@ -4,13 +4,16 @@ public class EmployeeMain {
 public static void main(String[] args) {  
 	Employee e1=new Employee();  
 	Employee e2=new Employee(); 
+	Employee e3=new Employee();
 	
 	
 	e1.insert(101,"Sean",45000);  
 	e2.insert(102,"John",25000);
+	e3.insert(102,"lisa",25000);
 	
 	e1.display();  
 	e2.display(); 
+	e3.display();
 	
 } 
 
